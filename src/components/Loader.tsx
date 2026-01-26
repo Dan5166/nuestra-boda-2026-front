f4a8604adaf17d9f1517570d46d3e1f22064467d
Danyael Natan Vasquez Ojeda <dnvasquez@iplacex.cl>
2026-01-26 10:29:59 -0300

    ADD Background to Loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,16 @@
+import FondoDesktop from "../assets/fondo-desktop.png";
+import FondoMovil from "../assets/fondo-movil.png";
+
 export default function Loader() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div
+      className="flex items-center justify-center h-screen w-screen"
+      style={{ backgroundImage: `url(${FondoDesktop})` }}
+    >
+      <div
+        className="fixed inset-0 bg-cover bg-center md:hidden"
+        style={{ backgroundImage: `url(${FondoMovil})` }}
+      />
       <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin" />
     </div>
   );
